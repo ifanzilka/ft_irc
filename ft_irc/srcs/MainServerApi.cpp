eb353d148ca43cf93f91a38c46c912a32c5bf2c5
Fanzil Nabiyarov <ifanzilka@MacBook-Air-Fanzil.local>
2022-07-12 02:47:21 +0300

    fix

--- ft_irc/srcs/MainServerApi.cpp
+++ ft_irc/srcs/MainServerApi.cpp
@@ -11,6 +11,11 @@ int main(int argc, char **argv)
     if (argc > 1)
 	{
 		ServerSelect serv("127.0.0.1", atoi(argv[1]));
+        while (1)
+        {
+            int a;
+            serv.WaitEvent(a);
+        }
 		//serv.Start();
 	}
 	else
